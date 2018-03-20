b138a3c33f17ce3fa727e65f2cbc0772df616789
kanwisher <kanwisher@gmail.com>
2018-03-19 22:27:14 -0400

    Base structure

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+
+            var customers = GetCustomers();
+            var customer = customers.Find(x => x.Id == Id);
+            if (customer != null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+            
+            
+        }
+
+        private static List<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() {Id = 1, Name = "David Kanwisher"},
+                new Customer() {Id = 2, Name = "Mirsada Haticic"}
+            };
+
+            return customers;
+
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            var movies = GetMovies();
+            var movie = movies.Find(x => x.Id == id);
+
+            return View(movie);
+
+        }
+
+        private static List<Movie> GetMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Flubber"},
+                new Movie() {Id = 2, Name = "Space Jam"}
+            };
+
+            return movies;
+
+        }
+
+    }
+}
